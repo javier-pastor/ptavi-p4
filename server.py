7deb518cc03ff8ddf8d2d1833b8d68998eb80939
Javier Pastor Tomas <jpastor@gamma13.aulas.gsyc.urjc.es>
2017-10-23 10:59:53 +0200

    Ejercicio 4 hecho de aquella manera (revisar por si se puede mejorar)

--- server.py
+++ server.py
@@ -22,9 +22,11 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    # Las dos comillas es lo mismo que poner localhost, podriamos poner
+    # cualquier IP
 
     print("Lanzando servidor UDP de eco...")
     try:
