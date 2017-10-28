0e35cd8e5565b6bf140a8f617dcfb236703f2ec9
Imac <imac@iMac-de-Imac.local>
2017-10-28 15:34:36 +0200

    Ejercicio 6 hecho

--- client.py
+++ client.py
@@ -10,16 +10,18 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = sys.argv[3:]
 frase = ''
+
+METHOD = sys.argv[3].upper()
+ADDR = sys.argv[4]
+
+LINE = METHOD + ' sip:' + ADDR + ' SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-    for palabra in LINE:
-        frase = frase + " " + palabra
-    my_socket.send(bytes(frase, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(LINE+ '\r\n', 'utf-8'))
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -10,28 +10,41 @@ import sys
 PORT = int(sys.argv[1])
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    addr_dic = {}
 
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"Hemos recibido tu peticion\r\n")
         print('CLIENT IP ',self.client_address[0])
         print('CLIENT PORT: ',self.client_address[1])
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+
+        while 1: # Read line by line what the client sent us
+            line = self.rfile.read()
+            if not line:
+                break
+            l_split = line.split()
+            l_split_method = str(l_split[0])[2:-1]
+            l_split_addr = str(l_split[1])[2:-1]
+            if l_split_method == 'REGISTER':
+                addr = l_split_addr.split(':')
+                self.addr_dic[addr[1]] = self.client_address[0]
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+
+        print ("Diccionario de direcciones:\r\n"+str(self.addr_dic))
+
 
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001
-    # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', PORT), EchoHandler)
-    # Las dos comillas es lo mismo que poner localhost, podriamos poner
-    # cualquier IP
+    # Listens at localhost ('') and port PORT
+    # and calls the SIPRegisterHandler class to manage the request
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
