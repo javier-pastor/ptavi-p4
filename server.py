2d7136f07a68a1584d47a16ed0e6d84d8c847d43
Imac <imac@iMac-de-Imac.local>
2017-10-29 11:20:40 +0100

    Ejercicio 8 terminado, faltaria repasar sintaxis

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     addr_dic = {}
+    json_dic = {}
 
     def handle(self):
         """
@@ -37,27 +38,30 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             l_split = line.split()
             method = str(l_split[0])[2:-1]
             sec_expires = int((l_split[4]))
+            dupla_addr = str(l_split[1])[2:-1].split(':')
+            addr = dupla_addr[1]
             if method == 'REGISTER':
-                dupla_addr = str(l_split[1])[2:-1].split(':')
-                addr = dupla_addr[1]
+
                 self.addr_dic[addr] = self.client_address[0]
+                self.json_dic[addr] = "address: " + self.client_address[0]
                 self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
             if sec_expires == 0:
                 del self.addr_dic[addr]
 
         print ("Registro de direcciones:\r\n"+str(self.addr_dic))
-        self.register2json();
+        self.register2json(addr, sec_expires);
 
-    def register2json(self):
-        print("YEI!")
+    def register2json(self, addr, sec_expires):
         strings = time.strftime("%Y,%m,%d,%H,%M,%S")
         t = strings.split(',')
         numbers = [ int(x) for x in t ] # año mes dia hora minuto segundo 0 - 5
-
+        hour = str(numbers[2]) + "-" + str(numbers[1]) + "-" + str(numbers[0]) + " " + str(numbers[3]) + ":" + str(numbers[4]) + ":" + str(numbers[5])
+        self.json_dic[addr] = str(self.json_dic[addr]) + " expires: " + hour + " +" + str(sec_expires)
         #nice = self.addr_dic + numbers
 
+
         with open("registered.json", 'w') as file:
-            json.dump(self.addr_dic, file)
+            json.dump(str(self.json_dic), file)
 
 
 if __name__ == "__main__":
