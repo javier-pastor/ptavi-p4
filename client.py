9b73cbc0e9e49b8f103c43cefc2a0afae05aed5b
Imac <imac@iMac-de-Imac.local>
2017-10-29 18:33:26 +0100

    Ejercicios 8 y 9 terminados, optimizados

--- client.py
+++ client.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
 
 import socket
 import sys
@@ -14,14 +11,13 @@ try:
         METHOD = sys.argv[3].upper()
         ADDR = sys.argv[4]
         EXPIRES = sys.argv[5]
+        int(EXPIRES)
 except IndexError:
         print ('Usage: client.py ip puerto register sip_address expires_value')
         raise SystemExit
 
 
-LINE = METHOD + ' sip:' + ADDR + ' SIP/2.0\r\n\r\n' + '\r\n' +'Expires: ' + EXPIRES + '\r\n\r\n'
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+LINE = METHOD + ' sip:' + ADDR + ' SIP/2.0\r\n\r\n' + '\r\n' + 'Expires: ' + EXPIRES + '\r\n\r\n'
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
--- server.py
+++ server.py
@@ -1,84 +1,66 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
 
-import socketserver
+
 import sys
-import time
 import json
-
-PORT = int(sys.argv[1])
-
+import socketserver
+import time
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
-    addr_dic = {}
-    json_dic = {}
+    dic_addr = {}
 
     def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        self.wfile.write(b"Hemos recibido tu peticion\r\n")
-        #print('CLIENT IP ',self.client_address[0])
-        #print('CLIENT PORT: ',self.client_address[1])
-
-        while 1: # Read line by line what the client sent us
-
-            line = self.rfile.read()
-            if not line:
-                break
-            l_split = line.split()
-            method = str(l_split[0])[2:-1]
-            sec_expires = int((l_split[4]))
-            dupla_addr = str(l_split[1])[2:-1].split(':')
-            addr = dupla_addr[1]
-            if method == 'REGISTER':
-
-                self.addr_dic[addr] = self.client_address[0]
-                self.json_dic[addr] = "address: " + self.client_address[0]
-                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
-            if sec_expires == 0:
-                del self.addr_dic[addr]
-
-        print ("Registro de direcciones:\r\n"+str(self.addr_dic))
-        self.register2json(addr, sec_expires);
-
-    def register2json(self, addr, sec_expires):
-        strings = time.strftime("%Y,%m,%d,%H,%M,%S")
-        t = strings.split(',')
-        numbers = [ int(x) for x in t ] # año mes dia hora minuto segundo 0 - 5
-        hour = str(numbers[2]) + "-" + str(numbers[1]) + "-" + str(numbers[0]) + " " + str(numbers[3]) + ":" + str(numbers[4]) + ":" + str(numbers[5])
-        self.json_dic[addr] = str(self.json_dic[addr]) + " expires: " + hour + " +" + str(sec_expires)
-        #nice = self.addr_dic + numbers
-
-        with open("registered.json", 'w') as file:
-            json.dump(str(self.json_dic), file)
-
+        line = self.rfile.read().decode('utf-8')
+        l_split = line.split()
         self.json2registered()
+        method = l_split[0]
+        sec_expires = l_split[3]
+        if method == 'REGISTER':
+            addr = l_split[1].split(':')[-1]
+            ip_user = self.client_address[0]
+            if sec_expires == 'Expires:':
+                expires = time.strftime('%Y-%m-%d %H:%M:%S',
+                                        time.gmtime(time.time() +
+                                                    int(l_split
+                                                        [4])))
+                self.dic_addr[addr] = [{'address': ip_user},
+                                                        {'expires': expires}]
+
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        self.check_expires()
+        self.register2json()
+        print(self.dic_addr)
+
+    def check_expires(self):
+        expired_users = []
+        for usuario in self.dic_addr:
+            time_now = time.strftime('%Y-%m-%d %H:%M:%S',
+                                     time.gmtime(time.time()))
+            if time_now >= self.dic_addr[usuario][1]['expires']:
+                expired_users.append(usuario)
+
+        for usuario in expired_users:
+            del self.dic_addr[usuario]
+
+    def register2json(self):
+        fich_json = open('registered.json', 'w')
+        codigo_json = json.dumps(self.dic_addr)
+        fich_json.write(codigo_json)
+        fich_json.close()
 
     def json2registered(self):
         try:
-            users = json.load(open('registered.json'))
-            print(users)
+            fich_json = open('registered.json', 'r')
+            self.dic_addr = json.load(fich_json)
         except:
-            print("El fichero no existe")
-
+            pass
 
 
 if __name__ == "__main__":
-    # Listens at localhost ('') and port PORT
-    # and calls the SIPRegisterHandler class to manage the request
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
-
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
-
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
